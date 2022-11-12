174adb2361f71c432f074ae59d08a5e90c0c2a7c
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-11-12 11:58:32 -0700

    Time: 285 ms (9.84%), Space: 42.2 MB (84.35%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (var i=0; i < nums.length; i++) {
+        for (var j=0; j < nums.length; j++) {
+            if (i === j) continue;
+            if (nums[i] + nums[j] === target) return [i, j];
+        }
+    }
+    return [-1, -1];
+};
