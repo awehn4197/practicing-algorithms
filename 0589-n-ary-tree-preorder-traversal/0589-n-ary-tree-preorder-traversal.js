5abb77b7b412f74848acd71fb0092fa1cc5dd82b
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-11-19 17:50:42 -0700

    Time: 201 ms (8.31%), Space: 55 MB (5.15%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,20 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+    if (!root) return [];
+    var arr = [root.val];
+    root.children.forEach(child => {
+        arr.push(...preorder(child));
+    });
+    return arr;
+};
