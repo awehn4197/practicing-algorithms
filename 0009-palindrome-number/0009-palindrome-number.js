b8ac9410f00c339cd005d712c210758e81b03ebb
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-11-21 22:21:31 -0700

    Time: 289 ms (71.05%), Space: 51.5 MB (23.70%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,11 +4,6 @@
  */
 var isPalindrome = function(x) {
     const xAsString = x.toString();
-    const digits = [...xAsString];
-    var index = 0;
-    while (index <= Math.floor(digits.length/2)) {
-        if (digits[index] != digits[digits.length-1-index]) return false;
-        index++;
-    }
-    return true;
+    const xAsStringReversed = [...xAsString].reverse().join('');
+    return xAsString === xAsStringReversed;
 };
