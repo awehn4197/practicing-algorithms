565c791b5c9a23fd5c4972a6130242f9dc296590
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-11-24 22:55:10 -0700

    Time: 2006 ms (5.00%), Space: 49.6 MB (5.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -3,7 +3,10 @@
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function(s, k) {    
+var characterReplacement = function(s, k) {
+    console.log(`s: ${s}`);
+    console.log(`k: ${k}`);
+
     var low = 1;
     var high = s.length + 1;
         
