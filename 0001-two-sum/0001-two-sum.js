91119b709ac749765e88f46709b377503e39259b
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-11-25 13:56:23 -0700

    Time: 151 ms (43.55%), Space: 48.4 MB (5.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (var i=0; i < nums.length; i++) {
-        for (var j=0; j < nums.length; j++) {
-            if (i === j) continue;
-            if (nums[i] + nums[j] === target) return [i, j];
+    var numsToIndices = new Map();
+    nums.forEach((num, index) => {
+        var indices = numsToIndices.get(num) || [];
+        indices.push(index);
+        numsToIndices.set(num, indices);
+    });
+    
+    for (let i = 0; i < nums.length; i++) {
+        var difference = target - nums[i];
+        var compl = numsToIndices.get(difference);
+        // console.log(`i: ${i}, nums[i]: ${nums[i]}, diff: ${difference}, compl: ${compl}`);
+        if (compl && Array.isArray(compl) && compl.length > 0) {
+            if (i === compl[0]) continue;
+            return [i, compl[0]];
         }
     }
+    
     return [-1, -1];
 };
