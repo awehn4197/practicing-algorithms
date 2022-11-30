d2fa0c38a529bdfc413962aa24c80838267d5a97
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-11-29 18:16:53 -0700

    Time: 66 ms (95.29%), Space: 44.2 MB (19.89%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,16 +3,26 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    var commonPrefix = "";
-    let i = 0;
-    while (strs.every(str => str.length >= i)) {
-        var nextChar = strs[0].charAt(i);
-        if (strs.every(str => str.charAt(i) === nextChar)) {
-            commonPrefix += nextChar;
-        } else {
-            return commonPrefix;
-        }
-        i++;
-    }
-    return commonPrefix;
+    return recursiveLCP(strs, 0, strs.length - 1);
 };
+
+var recursiveLCP = function(strs, startIndex, endIndex) {
+    if (startIndex === endIndex) return strs[startIndex];
+    var mid = Math.floor((startIndex+endIndex)/2);
+    var leftLCP = recursiveLCP(strs, startIndex, mid);
+    var rightLCP = recursiveLCP(strs, mid+1, endIndex);
+    return lcpComparison(leftLCP, rightLCP);
+}
+
+var lcpComparison = function(str1, str2) {
+    var n = Math.min(str1.length, str2.length);
+    var lcp = '';
+    for (let i = 0; i < n; i++) {
+        if (str1.charAt(i) === str2.charAt(i)) {
+            lcp += str1.charAt(i);
+        } else {
+            return lcp;
+        }
+    }
+    return lcp;
+}
