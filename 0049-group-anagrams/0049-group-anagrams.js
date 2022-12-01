deb7c6217a0e1a01cc9129f85e0d0a00043f7b2c
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-12-01 12:37:44 -0700

    Time: 191 ms (73.96%), Space: 54.3 MB (32.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,28 +3,47 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    var strsCopy = strs.slice();
-    strsCopy.map((str, i) => {
-        const sortedString = str.split('').sort().join('');
-        strsCopy[i] = sortedString;
+    
+    // map each string to an array [stringWithCharactersSorted, originalIndex]
+    var strsCopy = strs.map((str, index) => [[...str].sort().join(''), index]).sort();
+    
+    var anagramToIndices = new Map();
+    strsCopy.forEach(elem => {
+        var indices = anagramToIndices.get(elem[0]) || [];
+        indices.push(elem[1]);
+        anagramToIndices.set(elem[0], indices);
     });
-    const strsCopyAsSet = new Set(strsCopy); // get unique letter combinations
+    
     var output = [];
-    strsCopyAsSet.forEach(str => {
-        const matchingIndices = strsCopy.reduce((arr, nextString, index) => {
-            if (nextString === str) {
-                arr.push(index);
-            }
-            return arr;
-        }, []);
-        output.push(matchingIndices);
-    });
-    output.map((arr, i) => {
-        arr.map((index, i) => {
-            arr[i] = strs[index];
-        });
-    });
+    Array.from(anagramToIndices.keys()).forEach(key => {
+        var indices = anagramToIndices.get(key);
+        output.push(indices.map(index => strs[index]));
+    })
+    
     return output;
+    
+    // var strsCopy = strs.slice();
+    // strsCopy.map((str, i) => {
+    //     const sortedString = str.split('').sort().join('');
+    //     strsCopy[i] = sortedString;
+    // });
+    // const strsCopyAsSet = new Set(strsCopy); // get unique letter combinations
+    // var output = [];
+    // strsCopyAsSet.forEach(str => {
+    //     const matchingIndices = strsCopy.reduce((arr, nextString, index) => {
+    //         if (nextString === str) {
+    //             arr.push(index);
+    //         }
+    //         return arr;
+    //     }, []);
+    //     output.push(matchingIndices);
+    // });
+    // output.map((arr, i) => {
+    //     arr.map((index, i) => {
+    //         arr[i] = strs[index];
+    //     });
+    // });
+    // return output;
 };
 
 
