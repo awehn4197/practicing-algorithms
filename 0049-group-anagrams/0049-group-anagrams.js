ec36dff7e2cdc519e55fde6a9bf9c19a433c797b
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-12-01 13:11:27 -0700

    Time: 208 ms (63.31%), Space: 54.4 MB (29.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,26 +4,14 @@
  */
 var groupAnagrams = function(strs) {
     
-    // map each string to an array [stringWithCharactersSorted, originalString]
-    var strsCopy = strs.map(str => [[...str].sort().join(''), str]).sort();
+    var anagramMap = new Map();
     
-    var output = [];
-    var nextGroup = [];
-    
-    for (let i = 0; i < strsCopy.length; i++) {
-        if (i === 0) {
-            nextGroup.push(strsCopy[i][1]);
-        } else if (strsCopy[i-1][0] == strsCopy[i][0]) {
-            nextGroup.push(strsCopy[i][1]);
-        } else {
-            output.push(nextGroup);
-            nextGroup = [strsCopy[i][1]];
-        }
+    for (str of strs) {
+        var sortedStr = str.split('').sort().join('');
+        var anagrams = anagramMap.get(sortedStr) || [];
+        anagrams.push(str);
+        anagramMap.set(sortedStr, anagrams);
     }
     
-    if (nextGroup.length > 0) {
-        output.push(nextGroup);
-    }
-    
-    return output;
+    return Array.from(anagramMap.values());
 };
