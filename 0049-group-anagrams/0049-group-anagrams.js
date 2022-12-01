617a2ebc27e7ef98511c0d8b07cdcd60838a9cc5
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-12-01 12:53:15 -0700

    Time: 352 ms (9.14%), Space: 62.9 MB (5.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,9 +4,9 @@
  */
 var groupAnagrams = function(strs) {
     
-    // map each string to an array [stringWithCharactersSorted, originalIndex]
-    var strsCopy = strs.map((str, index) => [[...str].sort().join(''), index]).sort();
-    
+    // map each string to an array [stringWithCharactersSorted, originalString]
+    var strsCopy = strs.map(str => [[...str].sort().join(''), str]).sort();
+        
     var anagramToIndices = new Map();
     strsCopy.forEach(elem => {
         var indices = anagramToIndices.get(elem[0]) || [];
@@ -14,43 +14,5 @@ var groupAnagrams = function(strs) {
         anagramToIndices.set(elem[0], indices);
     });
     
-    var output = [];
-    Array.from(anagramToIndices.keys()).forEach(key => {
-        var indices = anagramToIndices.get(key);
-        output.push(indices.map(index => strs[index]));
-    })
-    
-    return output;
-    
-    // var strsCopy = strs.slice();
-    // strsCopy.map((str, i) => {
-    //     const sortedString = str.split('').sort().join('');
-    //     strsCopy[i] = sortedString;
-    // });
-    // const strsCopyAsSet = new Set(strsCopy); // get unique letter combinations
-    // var output = [];
-    // strsCopyAsSet.forEach(str => {
-    //     const matchingIndices = strsCopy.reduce((arr, nextString, index) => {
-    //         if (nextString === str) {
-    //             arr.push(index);
-    //         }
-    //         return arr;
-    //     }, []);
-    //     output.push(matchingIndices);
-    // });
-    // output.map((arr, i) => {
-    //     arr.map((index, i) => {
-    //         arr[i] = strs[index];
-    //     });
-    // });
-    // return output;
+    return [...anagramToIndices].map(entry => entry[1].flat())
 };
-
-
-/*
-make a shallow copy of string array (this is to maintain an original array with the strings in original order)
-treat each string as an array
-sort each string in alphabetical order
-use string equality to determine matching indices
-reference original array for original strings to match
-*/
