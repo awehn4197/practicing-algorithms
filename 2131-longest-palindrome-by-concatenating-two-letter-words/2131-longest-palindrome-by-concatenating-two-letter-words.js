d0554e6fb9a00c17f7fbf9247af0a6a2b7a9e72e
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2022-12-05 16:26:44 -0700

    Time: 138 ms (100.00%), Space: 54.2 MB (90.46%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.js
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.js
@@ -3,34 +3,34 @@
  * @return {number}
  */
 var longestPalindrome = function(words) {
+    var allTwoCharStrings = new Array(26);
+    for (let i = 0; i < 26; i++) {
+        allTwoCharStrings[i] = new Array(26);
+        allTwoCharStrings[i].fill(0);
+    }
     
-    var strCount = new Map();
     words.forEach(word => {
-        var key = [...word].sort().join('');
-        var val = strCount.get(key) || { fw: 0, bw: 0, isSymmetric: isStringSymmetric(word) };
-        areLettersAscending(word) ? val.fw++ : val.bw++;
-        strCount.set(key, val);
+        var i = word.charCodeAt(0) - 'a'.charCodeAt(0);
+        var j = word.charCodeAt(1) - 'a'.charCodeAt(0);
+        allTwoCharStrings[i][j]++;
     });
     
     var pairs = 0;
-    var centerpiece = false;
-    Array.from(strCount.values()).forEach(val => {
-        if (val.isSymmetric) {
-            pairs += Math.floor(val.bw/2);
-            centerpiece = centerpiece || val.bw%2 !== 0;
-        } else {
-            pairs += Math.min(val.fw, val.bw);;
-        }
-    });
+    var center = 0;
     
-    centerpiece = centerpiece ? 1 : 0;
-    return 2*(pairs*2 + centerpiece);
+    for (let i = 0; i < 26; i++) {
+        for (let j = 0; j < 26; j++) {
+            var currVal = allTwoCharStrings[i][j];
+            if (i === j) {
+                pairs += Math.floor(currVal/2);
+                center = center || currVal % 2;
+            } else {
+                var complVal = allTwoCharStrings[j][i];
+                pairs += Math.min(currVal, complVal);
+            }
+            allTwoCharStrings[i][j] = 0;
+        }
+    }
+    
+    return 2*(pairs*2 + center);
 };
-
-var areLettersAscending = (str) => {
-    return str.charCodeAt(1) - str.charCodeAt(0) > 0;
-}
-
-var isStringSymmetric = (str) => {
-    return str.charAt(0) === str.charAt(1);
-}
