df67b46633f1737d81bf1109e6179dacf794d79c
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2023-02-21 16:36:52 -0700

    Time: 73 ms (38.46%), Space: 44.6 MB (82.31%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.js
@@ -7,27 +7,13 @@ var singleNonDuplicate = function(nums) {
     let hi = nums.length - 1;
     
     while (low < hi) {
-        // console.log(`1. low: ${low}, hi: ${hi}`)
         let mid = (low+hi)/2;
-        let halvesAreEven = mid % 2 === 0;
-        
-        if (nums[mid] === nums[mid-1]) {
-            if (halvesAreEven) {
-                hi = mid - 2;
-            } else {
-                low = mid + 1;
-            }
-        } else if (nums[mid] === nums[mid+1]) {
-            if (halvesAreEven) {
-                low = mid + 2;
-            } else {
-                hi = mid - 1;
-            }
+        if (mid % 2 === 1) mid--;
+        if (nums[mid] === nums[mid+1]) {
+            low = mid+2;
         } else {
-            return nums[mid];
+            hi = mid;
         }
-        // console.log(`2. low: ${low}, hi: ${hi}`)
-
     }
     
     return nums[low];
