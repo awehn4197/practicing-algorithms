edf16a32a141a712628a4849252ba81fef728ff0
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2023-03-14 15:11:07 -0700

    Time: 2 ms (64.90%), Space: 40.5 MB (38.31%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(s.charAt(i));
+                    break;
+                case ')':
+                    if (!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (!stack.isEmpty() && stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                    break;
+                    
+            }
+        }
+        return stack.isEmpty();
+    }
+}
