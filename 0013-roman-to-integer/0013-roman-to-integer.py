a26449185441c4da043608e60d518fb71eb1e743
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2023-11-27 20:02:20 -0700

    Time: 58 ms (24.70%), Space: 16.3 MB (22.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,38 +1,20 @@
-def map_char(c: str) -> int:
-    match c:
-        case 'I':
-            return 1
-        case 'V':
-            return 5
-        case 'X':
-            return 10
-        case 'L':
-            return 50
-        case 'C':
-            return 100
-        case 'D':
-            return 500
-        case 'M':
-            return 1000
-        case _:
-            return
-
 class Solution:
     def romanToInt(self, s: str) -> int:
+        d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
         sum = 0
-        i = 0
-        while i < len(s):
-            curr = s[i]
-            if (i < len(s) - 1):
-                nxt = s[i+1]
-                if (map_char(curr) < map_char(nxt)):
-                    sum += map_char(nxt) - map_char(curr)
-                    i += 2
-                else:
-                    sum += map_char(curr)
-                    i += 1
+        for i in range(len(s)-1):
+            print(f"i: {i}, s[i]: {s[i]}")
+            if d[s[i]] < d[s[i+1]]:
+                sum -= d[s[i]]
             else:
-                sum += map_char(curr)
-                i += 1
-        return sum
-        
+                sum += d[s[i]]
+        return sum + d[s[len(s)-1]]
+            
