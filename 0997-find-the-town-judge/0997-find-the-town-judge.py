0eb02cbd8156024b2bec1f43aab7b502bc2d527e
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2023-11-29 19:33:38 -0700

    Time: 668 ms (61.33%), Space: 22.3 MB (10.31%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trustsSomeone = [False]*n
+        peopleTrustedBy = [0]*n
+        for [truster, trustee] in trust:
+            trustsSomeone[truster-1] = True
+            peopleTrustedBy[trustee-1] += 1
+        for idx, count in enumerate(peopleTrustedBy):
+            # print(f"count: {count}, idx: {idx}")
+            if count == n-1 and not trustsSomeone[idx]:
+                return idx+1
+            
+        return -1
