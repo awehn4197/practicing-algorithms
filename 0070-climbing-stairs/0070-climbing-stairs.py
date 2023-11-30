06c0de66074922d2ad041b08c87cc0b0c006579b
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2023-11-30 11:14:07 -0700

    Time: 46 ms (6.12%), Space: 16.3 MB (41.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,18 @@
+import math
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+        noSteps = n
+        maxNoTwoSteps = noSteps // 2
+        sum = 0
+        for noTwoSteps in range(maxNoTwoSteps + 1):
+            sum += math.factorial((noSteps - 2*noTwoSteps) + noTwoSteps)/(math.factorial(noSteps-2*noTwoSteps)*math.factorial(noTwoSteps))
+        return int(sum)
+        # if n <= 2:
+        #     return n
+        # dp = [0]*(n+1)
+        # dp[1] = 1
+        # dp[2] = 2
+        # for i in range(3, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
+        # return dp[n]
