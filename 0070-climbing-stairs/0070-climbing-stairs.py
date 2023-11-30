a2f896f806f5191d27b35bda5992cb1027255309
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2023-11-30 11:32:03 -0700

    Time: 36 ms (70.65%), Space: 16.2 MB (73.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,26 @@
 import math
 
+factorial_dict = {
+    1: 1,
+    2: 2
+}
+
+def get_factorial(z):
+    lookup = factorial_dict.get(z)
+    if lookup:
+        return lookup
+    else:
+        f = math.factorial(z)
+        factorial_dict[z] = f
+        return f    
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         noSteps = n
         maxNoTwoSteps = noSteps // 2
         sum = 0
         for noTwoSteps in range(maxNoTwoSteps + 1):
-            sum += math.factorial((noSteps - 2*noTwoSteps) + noTwoSteps)/(math.factorial(noSteps-2*noTwoSteps)*math.factorial(noTwoSteps))
+            sum += get_factorial((noSteps - 2*noTwoSteps) + noTwoSteps)/(get_factorial(noSteps-2*noTwoSteps)*get_factorial(noTwoSteps))
         return int(sum)
         # if n <= 2:
         #     return n
