5db7d053b61885e40664d5a85b00bf6932af939a
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-01-12 14:54:34 -0800

    Time: 37 ms (66.52%), Space: 17.3 MB (31.06%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,6 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        if len(word1) == 0:
-            return word2
+        return self.mergeAlternatelyWithItr(word1, word2, 0)
+        
+        
+    def mergeAlternatelyWithItr(self, word1: str, word2: str, itr: int) -> str:
+        idx = itr // 2
+        if len(word1[idx:]) == 0:
+            return word2[idx:]
         else:
-            return word1[0] + self.mergeAlternately(word2, word1[1:])
+            return word1[idx] + self.mergeAlternately(word2[idx:], word1[idx+1:])
+        
