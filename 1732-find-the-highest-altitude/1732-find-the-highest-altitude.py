d70fcea0c4f8a39de58081e8051c56fa41e0d7a1
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-01-24 22:31:26 -0700

    Time: 32 ms (94.54%), Space: 16.5 MB (59.64%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -2,7 +2,10 @@ class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         max_alt = 0
         current = 0
-        for g in gain:
-            current += g
+        for i in range(len(gain)):
+            current += gain[i]
             max_alt = max(max_alt, current)
+        # for g in gain:
+        #     current += g
+        #     max_alt = max(max_alt, current)
         return max_alt
