60cf4a8349918fbbfa1d096750cd75e1663c91d3
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-02-28 19:50:15 -0700

    Time: 35 ms (75.25%), Space: 17.5 MB (98.60%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -10,14 +10,11 @@ class Solution:
         
         prev_node = None
         curr_node = head
-        next_node = head.next
         
-        while next_node:
-            next_next_node = next_node.next
-            next_node.next = curr_node
+        while curr_node:
+            next_node = curr_node.next
             curr_node.next = prev_node
             prev_node = curr_node
             curr_node = next_node
-            next_node = next_next_node
         
-        return curr_node
+        return prev_node
