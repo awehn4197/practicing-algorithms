6112735f1602d021b9b6a30598455fec20e5e384
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-05-07 10:22:02 -0700

    Time: 55 ms (57.98%), Space: 50.5 MB (54.29%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1)
+    // const set2 = new Set(nums2)
+    
+    return [...new Set(nums2.filter(num => set1.has(num)))]
+    
+    
+    // return set1.filter(num => set2.has(num))
+};
