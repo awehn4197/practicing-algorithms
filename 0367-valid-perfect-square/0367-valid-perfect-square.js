3e66797efd20564bc7a3563bc3a38203f227bd41
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-05-07 10:36:35 -0700

    Time: 46 ms (86.34%), Space: 48.5 MB (80.41%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,5 +3,24 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    return Math.floor(Math.sqrt(num)) === Math.ceil(Math.sqrt(num))
+    if (num === 1) {
+        return true
+    }
+    let left = 0
+    let right = num/2
+    let mid
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2)
+        // console.log(`left: ${left}, right: ${right}, mid: ${mid}`)
+        square = mid * mid
+        if (square === num) {
+            return true
+        }
+        if (square < num) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
 };
