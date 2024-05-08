5ee1465c9c8eae4e4117d4fd642bea484dfc66d4
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-05-08 16:16:15 -0700

    Time: 107 ms (31.96%), Space: 63.9 MB (14.29%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+    const candyCount = new Map()
+    candyType.forEach(type => {
+        const count = candyCount.get(type) ?? 0
+        candyCount.set(type, count+1)
+    })
+    // console.log(candyCount)
+    // console.log(candyCount.size)
+    // console.log(candyType.length/2)
+    return Math.min(candyType.length/2, candyCount.size)
+};
