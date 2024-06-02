c783286df74bdca1358cd6b4b3156e62396c5f70
awehn4197 <83041580+awehn4197@users.noreply.github.com>
2024-06-02 12:16:10 -0700

    Time: 72 ms (98.29%), Space: 55.5 MB (94.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -4,47 +4,33 @@
  * @return {boolean}
  */
 var closeStrings = function(word1, word2) {
+    
     if (word1.length !== word2.length) {
         return false
     }
     
-    let w1Dict = new Map()
+    const offset = 97;
+    const count1 = Array(26).fill(0);
+    const count2 = Array(26).fill(0);
     
-    for (let i=0; i < word1.length; i++) {
-        let ch = word1[i]
-        let chCount = w1Dict.get(ch) ?? 0
-        w1Dict.set(ch, chCount+1)
+    for (let i = 0; i < word1.length; i++) {
+        count1[word1.charCodeAt(i)-offset]++
+        count2[word2.charCodeAt(i)-offset]++
     }
     
-    let w2Dict = new Map()
-    
-    for (let i=0; i < word2.length; i++) {
-        let ch = word2[i]
-        let chCount = w2Dict.get(ch) ?? 0
-        w2Dict.set(ch, chCount+1)
-    }
-    
-    w1Set = new Set(w1Dict.keys())
-    w2Set = new Set(w2Dict.keys())
-    
-    if (w1Set.size !== w2Set.size) {
-        return false
-    }
-    
-    for (let w1Ch of w1Set) {
-        if (!w2Set.has(w1Ch)) {
+    for (let i = 0; i < count1.length; i++) {
+        if (count1[i] === 0 && count2[i] > 0) {
+            return false
+        }
+        if (count2[i] === 0 && count1[i] > 0) {
             return false
         }
     }
     
-    // Check that counts are the same
-    // as long as the two strings are composed of the same characters
-    // and there are the same number of characters to map to then
-    // they are close
-    const w1Counts = Array.from(w1Dict.values()).sort((a, b) => b-a)
-    const w2Counts = Array.from(w2Dict.values()).sort((a, b) => b-a)
-    for (let i = 0; i < w1Counts.length; i++) {
-        if (w1Counts[i] !== w2Counts[i]) {
+    count1.sort()
+    count2.sort()
+    for (let i = 0; i < count1.length; i++) {
+        if (count1[i] !== count2[i]) {
             return false
         }
     }
